be678d90564fabeb87f79e8101761c959ac56780
Saajan <saajan.joshi@sagarmatha.edu.np>
2021-07-28 23:35:30 +0545

    health check

--- src/main/java/com/store/storeapp/controller/HomeController.java
+++ src/main/java/com/store/storeapp/controller/HomeController.java
@@ -4,13 +4,14 @@ import net.minidev.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping
     public HttpEntity home(){
         JSONObject obj = new JSONObject();
         obj.put("Application","shop");
